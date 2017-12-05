3d0b47cfbcc95ef0db821651f97c652ac4ec6be0
ikhlas firlana <ifirlana@gmail.com>
2017-12-05 11:19:10 +0700

    tutorial 2 basic web apps

--- main.go
+++ main.go
@@ -1,33 +1,78 @@
 package main
 
 import (
-	"html/template"
-	"log"
-	"os"
+	//"html/template"
+	//"log"
+	//"os"
 	//"io/ioutil"
+	"net/http"
+	//"fmt"
+	"time"
+	"encoding/json"
 )
 
+//type User struct {
+//	FirstName string
+//	LastName string
+//	Email string
+//	Age int
+//}
+
+//func (u User) IsOld() bool  {
+//	return u.Age > 30
+//}
 type User struct {
-	FirstName string
-	LastName string
-	Email string
-	Age int
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Email string `json:"email"`
+	CreatedAt time.Time
 }
 
-func (u User) IsOld() bool  {
-	return u.Age > 30
+type HomePageHandler struct{}
+
+func (h *HomePageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	//name := r.URL.Query().Get("name")
+	//if name == "" {
+	//	name = "World";
+	//}
+	//
+	//fmt.Fprintf(w, "Hello %s!", name)
+	user := new(User)
+	json.NewDecoder(r.Body).Decode(user)
+	user.CreatedAt = time.Now()
+
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	data, _:= json.Marshal(user)
+	w.Write(data)
 }
 
 func main()  {
-	u := User{"Mark", "Zuckerberg", "mark@example.com", 37}
-	//body, _ := ioutil.ReadFile("templates/file.tmpl")
-	//tmpl, err := template.New("Some Name").Parse(string(body))
+	//u := User{"Mark", "Zuckerberg", "mark@example.com", 37}
+	////body, _ := ioutil.ReadFile("templates/file.tmpl")
+	////tmpl, err := template.New("Some Name").Parse(string(body))
+	//
+	//tmpl, _ := template.ParseFiles("templates/file2.html", "templates/file3.html")
+	//err := tmpl.ExecuteTemplate(os.Stdout, "file2.html", u)
+	//
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//tmpl.Execute(os.Stdout, u)
 
-	tmpl, _ := template.ParseFiles("templates/file2.html", "templates/file3.html")
-	err := tmpl.ExecuteTemplate(os.Stdout, "file2.html", u)
+	//http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	//	fmt.Fprintln(writer, "Hello World!")
+	//})
+	//
+	//barHandle := func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprintln(w, "Hello Bar!")
+	//}
+	//
+	//http.HandleFunc("/bar", barHandle)
 
-	if err != nil {
-		log.Panic(err)
-	}
-	tmpl.Execute(os.Stdout, u)
+	http.Handle("/", &HomePageHandler{})
+
+	http.ListenAndServe(":3000",nil)
+
 }
