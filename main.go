6bf3fa6d4767a6f92b27140d5116ce703e6487fa
ikhlas firlana <ifirlana@gmail.com>
2017-12-11 10:53:12 +0700

    no message

--- main.go
+++ main.go
@@ -2,35 +2,9 @@ package main
 
 
 import (
-	"html/template"
-	"net/http"
-	"log"
+	"fmt"
 )
 
-type Gopher struct {
-	Name string
-}
-
-func hello(w http.ResponseWriter, r *http.Request)  {
-	var gophername string
-	gophername = r.URL.Query().Get("name");
-
-	if (gophername == "") {
-		gophername = "GOPHER"
-	}
-	gopher := Gopher{Name: gophername}
-	renderTemplate(w, "./template/index.html", gopher)
-}
-
-func renderTemplate(w http.ResponseWriter, templateFile string, templateData interface{})  {
-	t, err := template.ParseFiles(templateFile)
-	if err != nil {
-		log.Fatal("Error Encountered while parse the template: ", err)
-	}
-	t.Execute(w, templateData)
-}
-
 func main() {
-	http.HandleFunc("/", hello);
-	http.ListenAndServe(":8080", nil);
+	fmt.Println("hello world")
 }
